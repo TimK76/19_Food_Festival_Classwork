608f4a6ae8d769d2d9e2a6366061c2f6fed948e8
TimK76 <haadelphos2001@gmail.com>
2022-05-27 14:10:56 -0400

    updated service-worker and webpack.config. But I'm very tired of checking my code against the code snippets at the end of these lessons only to find that the code snippets are very different, which makes me think I've fucked it up somehow, only to change things to be like the snippets I'm given that are supposed to represent actual working code but haven't been updated, so that when I run my now updated code with the code snippets it doesn't work. Then reverting back to what I had and it works.

--- webpack.config.js
+++ webpack.config.js
@@ -2,49 +2,57 @@ const path = require("path");
 const webpack = require("webpack");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
-module.exports = {
-  entry: {
-    app: "./assets/js/script.js",
-    events: "./assets/js/events.js",
-    schedule: "./assets/js/schedule.js",
-    tickets: "./assets/js/tickets.js"
-  },
-  output: {
-    path: __dirname + "/dist",
-    filename: "[name].bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jpg$/i,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              esModule: false,
-              name (file) {
-                return "[path][name].[ext]"
-              },
-              publicPath: function(url) {
-                return url.replace("../", "/assets/")
-              }
-            }  
-          },
-          {
-            loader: 'image-webpack-loader'
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [
-      new webpack.ProvidePlugin({
-          $: "jquery",
-          jQuery: "jquery"
-      }),
-      new BundleAnalyzerPlugin({
-        analyzerMode: "static", // the report outputs to an HTML file in the dist foler
-      })
-  ],
-  mode: "development",
-};
+
+  module.exports = {
+    entry: {
+      app: "./assets/js/script.js",
+      events: "./assets/js/events.js",
+      schedule: "./assets/js/schedule.js",
+      tickets: "./assets/js/tickets.js"
+    },
+    output: {
+      path: __dirname + "/dist",
+      filename: "[name].bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jpg$/i,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                esModule: false,
+                name (file) {
+                  return "[path][name].[ext]"
+                },
+                publicPath: function(url) {
+                  return url.replace("../", "/assets/")
+                }
+              }  
+            },
+            {
+              loader: 'image-webpack-loader'
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+        new BundleAnalyzerPlugin({
+          analyzerMode: "static", // the report outputs to an HTML file in the dist foler
+        })
+    ],
+      devServer: {
+        static: {
+          directory: path.join(__dirname, './'),
+        },
+        compress: true,
+        port: 8080,
+      },
+      mode: 'development'
+  };
